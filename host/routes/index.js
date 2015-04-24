b9d1fc9019ad9181648cf31d6cdd78e935a580d3
Main Khan <mukhan@umass.edu>
2015-04-24 19:24:41 +0000

    trying to figure out dojo, commented out index.js

--- host/routes/index.js
+++ host/routes/index.js
@@ -1,52 +1,68 @@
-var express = require('express');
-var router = express.Router();
+require([
+		"dojo/store/Memory",
+		"dojo/store/JsonRest",
+		"dojo/store/Observable",
+		"dojo/store/Cache"
+		],
+		
+	function(Memory, JsonRest, Observable, Cache){
+		/* some data */
+		var store = new JsonRest({
+			target: /* some resource */
+		});
+		store = new Memory({}); /* some stuff */
+});
 
-// require(["dojo/router"], function(router){
-//   router.register("/something/:id", function(evt){
-//     // Will fire when the hash matches
-//     // evt.params.id will contain what is passed in :id
-//   });
 
-//   // Startup must be called in order to "activate" the router
-//   router.startup();
+// var express = require('express');
+// var router = express.Router();
+
+// // require(["dojo/router"], function(router){
+// //   router.register("/something/:id", function(evt){
+// //     // Will fire when the hash matches
+// //     // evt.params.id will contain what is passed in :id
+// //   });
+
+// //   // Startup must be called in order to "activate" the router
+// //   router.startup();
+// // });
+
+// // # Server Side Routes For Users Not Logged In
+
+// // ## home
+// // The home page view that will allow users to log in or create a new account
+// router.get('/', function (req, res){
+
 // });
 
-// # Server Side Routes For Users Not Logged In
+// // ## login
+// // The login page for users to sign in
+// router.get('/login', function (req, res){
 
-// ## home
-// The home page view that will allow users to log in or create a new account
-router.get('/', function (req, res){
+// });
 
-});
+// // ## new
+// // The page to make a new StuddyBuddy account
+// router.get('/new', function(req, res){
 
-// ## login
-// The login page for users to sign in
-router.get('/login', function (req, res){
+// });
 
-});
+// // ## forgot-password
+// // The page to recover a lost password
+// router.get('/forgot-password', function(req,res){
 
-// ## new
-// The page to make a new StuddyBuddy account
-router.get('/new', function(req, res){
+// });
 
-});
+// // ## auth
+// // Performs basic user authentication
+// router.post('/auth', function(req, res){
 
-// ## forgot-password
-// The page to recover a lost password
-router.get('/forgot-password', function(req,res){
+// });
 
-});
+// // ## process
+// // Processes information to create a new account
+// router.post('/process', function(req,res){
 
-// ## auth
-// Performs basic user authentication
-router.post('/auth', function(req, res){
+// });
 
-});
-
-// ## process
-// Processes information to create a new account
-router.post('/process', function(req,res){
-
-});
-
-module.exports = router;
+// module.exports = router;

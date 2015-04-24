6dc9f37712264006e2e7ebf012189a8c2ea24247
Gary P White <GaryPWhite@live.com>
2015-04-24 15:16:41 -0400

    Created login screen. NOTE: Login is a different page from the rest of the website. This is a better design in our minds because we will be able to load the bulk resources after we have confirmed information about the user.

---
+++ host/wscript/script.js
@@ -0,0 +1,29 @@
+require([
+	'dojo/dom',
+	'dojo/dom-construct',
+	'dojo/on',
+	'dojo/_base/fx',
+	'dojo/mouse',
+	'dojo/dom-style',
+	'dojo/query',
+    'dojo/domReady!'
+	], function(dom, domConstruct, on, fx, mouse, domStyle, query){
+		// fade elements
+		var kanye = fx.fadeOut({node: "fadeout",duration:400});
+		var west = fx.fadeIn({node: "fadein",duration:400});
+		var eynak = fx.fadeIn({node: "fadeout",duration:400});
+		var tsew = fx.fadeOut({node: "fadein",duration:400});
+		var fadebox = dom.byId("fadediv");
+		
+		on(fadebox, mouse.enter, function(evt){
+			kanye.play();
+			west.play();
+		});
+		on(fadebox, mouse.leave, function(evt){
+			eynak.play();
+			tsew.play();
+		});
+		
+		
+		
+	});

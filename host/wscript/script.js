78252ef2b2e6c948fb9e2e9009948d97ae7552fb
Gary P White <GaryPWhite@live.com>
2015-04-27 23:09:13 -0400

    and another one srsly

--- host/wscript/script.js
+++ host/wscript/script.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-require([
-	'dojo/dom',
-	'dojo/dom-construct',
-	'dojo/on',
-	'dojo/_base/fx',
-	'dojo/mouse',
-	'dojo/dom-style',
-	'dojo/query',
-    'dojo/domReady!'
-	], function(dom, domConstruct, on, fx, mouse, domStyle, query){
-		// fade elements
-		var kanye = fx.fadeOut({node: "fadeout",duration:400});
-		var west = fx.fadeIn({node: "fadein",duration:400});
-		var eynak = fx.fadeIn({node: "fadeout",duration:400});
-		var tsew = fx.fadeOut({node: "fadein",duration:400});
-		var fadebox = dom.byId("fadediv");
-		
-		on(fadebox, mouse.enter, function(evt){
-			kanye.play();
-			west.play();
-		});
-		on(fadebox, mouse.leave, function(evt){
-			eynak.play();
-			tsew.play();
-		});
-		
-		
-		
-	});
-
-//moved this here from index.js anc user.js -Grayson
-
-require([
-		"dojo/store/Memory",
-		"dojo/store/JsonRest",
-		"dojo/store/Observable",
-		"dojo/store/Cache"
-	],
-
-	function(Memory, JsonRest, Observable, Cache){
-		/* some data */
-		var store = new JsonRest({
-			target: ""/* some resource */
-		});
-		store = new Memory({}); /* some stuff */
-	});
-=======
 require([
 	'dojo/dom',
 	'dojo/dom-construct',
@@ -74,6 +26,3 @@ require([
 		
 		
 });
-
-//moved this here from index.js anc user.js -Grayson
->>>>>>> 828ffed3b9b767687aa9def4930310364c5a6d1d
